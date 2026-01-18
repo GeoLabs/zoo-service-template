a8d88b8c4b915b63a772ad302bcd826fd402afff
Gérald Fenoy <gerald.fenoy@geolabs.fr>
2026-01-18 18:08:36 +0100

    Initial version inheriting from zoo-template-common

--- {{cookiecutter.service_name}}/service.py
+++ {{cookiecutter.service_name}}/service.py
@@ -24,285 +24,34 @@ import sys
 import traceback
 import yaml
 import json
-import boto3  # noqa: F401
-import botocore
 from loguru import logger
-from urllib.parse import urlparse
-from botocore.exceptions import ClientError
-from botocore.client import Config
 from pystac import read_file, Collection, Catalog
-from pystac.stac_io import DefaultStacIO, StacIO
 from pystac.item_collection import ItemCollection
-from zoo_calrissian_runner import ExecutionHandler, ZooCalrissianRunner
+from pystac.stac_io import StacIO
+from zoo_calrissian_runner import ZooCalrissianRunner
+from zoo_template_common import CommonExecutionHandler, CustomStacIO
 
 
 logger.remove()
 logger.add(sys.stderr, level="INFO")
 
-
-class CustomStacIO(DefaultStacIO):
-    """Custom STAC IO class that uses boto3 to read from S3."""
-
-    def __init__(self):
-        self.session = botocore.session.Session()
-        self.s3_client = self.session.create_client(
-            service_name="s3",
-            region_name=os.environ["AWS_S3_REGION"],
-            endpoint_url=os.environ["AWS_S3_ENDPOINT"],
-            aws_access_key_id=os.environ["AWS_ACCESS_KEY_ID"],
-            aws_secret_access_key=os.environ["AWS_SECRET_ACCESS_KEY"],
-        )
-
-    def read_text(self, source, *args, **kwargs):
-        parsed = urlparse(source)
-        if parsed.scheme == "s3":
-            return (
-                self.s3_client.get_object(Bucket=parsed.netloc, Key=parsed.path[1:])[
-                    "Body"
-                ]
-                .read()
-                .decode("utf-8")
-            )
-        else:
-            return super().read_text(source, *args, **kwargs)
-
-    def write_text(self, dest, txt, *args, **kwargs):
-        parsed = urlparse(dest)
-        if parsed.scheme == "s3":
-            self.s3_client.put_object(
-                Body=txt.encode("UTF-8"),
-                Bucket=parsed.netloc,
-                Key=parsed.path[1:],
-                ContentType="application/geo+json",
-            )
-        else:
-            super().write_text(dest, txt, *args, **kwargs)
-
-
 StacIO.set_default(CustomStacIO)
 
 
-class SimpleExecutionHandler(ExecutionHandler):
+class SimpleExecutionHandler(CommonExecutionHandler):
+    """Simple execution handler for basic ZOO workflows.
+    
+    Extends CommonExecutionHandler with specific storage platform configuration.
+    """
+
     def __init__(self, conf, outputs):
-        super().__init__()
-        self.conf = conf
-        self.outputs = outputs
-        self.results = None
-
-    def pre_execution_hook(self):
-
-        logger.info("Pre execution hook")
-
-    def setOutput(self, outputName, values):
-        output=self.outputs[outputName]
-        logger.info(f"Read catalog from STAC Catalog URI: {output} -> {values}")
-
-        if not(isinstance(values[outputName], list)):
-            logger.info(f"values[{outputName}] is not a list, tranform to an array")
-            values[outputName]=[values[outputName]]
-
-        items = []
-
-        for i in range(len(values[outputName])):
-            if values[outputName][i] is None:
-                break
-            cat: Catalog  = read_file(values[outputName][i]["value"])
-
-            collection_id = self.get_additional_parameters()["sub_path"]
-
-            logger.info(f"Create collection with ID {collection_id}")
-
-            collection = None
-
-            try:
-                logger.info(f"Catalog : {dir(cat)}")
-                collection: Collection = next(cat.get_all_collections())
-            except Exception as e:
-                logger.error("No collection found in the output catalog")
-                output["collection"] = json.dumps({}, indent=2)
-                return
-
-            logger.info("Got collection {collection.id} from processing outputs")
-            
-            for item in collection.get_all_items():
-
-                logger.info("Processing item {item.id}")
-                
-                for asset_key in item.assets.keys():
-
-                    logger.info(f"Processing asset {asset_key}")
-
-                    temp_asset = item.assets[asset_key].to_dict()
-                    temp_asset["storage:platform"] = "eoap"
-                    temp_asset["storage:requester_pays"] = False
-                    temp_asset["storage:tier"] = "Standard"
-                    temp_asset["storage:region"] = self.get_additional_parameters()[
-                        "region_name"
-                    ]
-                    temp_asset["storage:endpoint"] = self.get_additional_parameters()[
-                        "endpoint_url"
-                    ]
-                    item.assets[asset_key] = item.assets[asset_key].from_dict(temp_asset)
-
-                item.collection_id = collection_id
-
-                items.append(item.clone())
-
-        item_collection = ItemCollection(items=items)
-
-        logger.info("Created feature collection from items")
-
-        # Trap the case of no output collection
-        if item_collection is None:
-            logger.error("The output collection is empty")
-            output["collection"] = json.dumps({}, indent=2)
-            return
-
-        # Set the feature collection to be returned
-        output["collection"] = item_collection.to_dict()
-        output["collection"]["id"] = collection_id
-
-    def post_execution_hook(self, log, output, usage_report, tool_logs):
-
-        # unset HTTP proxy or else the S3 client will use it and fail
-        os.environ.pop("HTTP_PROXY", None)
-
-        os.environ["AWS_S3_REGION"] = self.get_additional_parameters()["region_name"]
-        os.environ["AWS_S3_ENDPOINT"] = self.get_additional_parameters()["endpoint_url"]
-        os.environ["AWS_ACCESS_KEY_ID"] = self.get_additional_parameters()["aws_access_key_id"]
-        os.environ["AWS_SECRET_ACCESS_KEY"] = self.get_additional_parameters()["aws_secret_access_key"]
-
-        logger.info("Post execution hook")
-
-        StacIO.set_default(CustomStacIO)
-
-        for i in self.outputs:
-            logger.info(f"Output {i}: {self.outputs[i]}")
-            if "mimeType" in self.outputs[i]:
-                self.setOutput(i,output)
-            else:
-                logger.warning(f"Output {i} has no mimeType, skipping...")
-                self.outputs[i]["value"] = str(output[i])
-
-
-    @staticmethod
-    def local_get_file(fileName):
-        """
-        Read and load the contents of a yaml file
-
-        :param yaml file to load
-        """
-        try:
-            with open(fileName, "r") as file:
-                data = yaml.safe_load(file)
-            return data
-        # if file does not exist
-        except FileNotFoundError:
-            return {}
-        # if file is empty
-        except yaml.YAMLError:
-            return {}
-        # if file is not yaml
-        except yaml.scanner.ScannerError:
-            return {}
-
-    def get_pod_env_vars(self) -> Dict[str, str]:
-        # This method is used to set environment variables for the pod
-        # spawned by calrissian.
-
-        logger.info("get_pod_env_vars")
-
-        env_vars: Dict[str, str] = {}
-        env_vars = self.conf.get("pod_env_vars", {})
-
-        return env_vars
-
-    def get_pod_node_selector(self) -> Dict[str, str]:
-        # This method is used to set node selectors for the pod
-        # spawned by calrissian.
-
-        logger.info("get_pod_node_selector")
-        node_selector: Dict[str, str] = {}
-        node_selector = self.conf.get("pod_node_selector", {})
-
-        logger.info(f"node_selector: {node_selector.keys()}")
-
-        return node_selector
+        super().__init__(conf, outputs)
 
     def get_additional_parameters(self) -> Dict[str, str]:
-        # sets the additional parameters for the execution
-        # of the wrapped Application Package
-
-        logger.info("get_additional_parameters")
-        additional_parameters: Dict[str, str] = {}
-        additional_parameters = self.conf.get("additional_parameters", {})
-
-        additional_parameters["sub_path"] = self.conf["lenv"]["usid"]
-
-        logger.info(f"additional_parameters: {additional_parameters.keys()}")
-
+        """Get additional parameters with eoap storage platform."""
+        additional_parameters = super().get_additional_parameters()
+        additional_parameters["storage_platform"] = "eoap"
         return additional_parameters
-
-    def handle_outputs(self, log, output, usage_report, tool_logs):
-        """
-        Handle the output files of the execution.
-
-        :param log: The application log file of the execution.
-        :param output: The output file of the execution.
-        :param usage_report: The metrics file.
-        :param tool_logs: A list of paths to individual workflow step logs.
-
-        """
-
-        try:
-            logger.info("handle_outputs")
-
-            self.conf["main"]["tmpUrl"] = self.conf["main"]["tmpUrl"].replace(
-                "temp/", self.conf["auth_env"]["user"] + "/temp/"
-            )
-            services_logs = [
-                {
-                    "url": os.path.join(
-                        self.conf["main"]["tmpUrl"],
-                        f"{self.conf['lenv']['Identifier']}-{self.conf['lenv']['usid']}",
-                        os.path.basename(tool_log),
-                    ),
-                    "title": f"Tool log {os.path.basename(tool_log)}",
-                    "rel": "related",
-                }
-                for tool_log in tool_logs
-            ]
-            cindex=0
-            if "service_logs" in self.conf:
-                cindex=1
-            for i in range(len(services_logs)):
-                okeys = ["url", "title", "rel"]
-                keys = ["url", "title", "rel"]
-                if cindex > 0:
-                    for j in range(len(keys)):
-                        keys[j] = keys[j] + "_" + str(cindex)
-                if "service_logs" not in self.conf:
-                    self.conf["service_logs"] = {}
-                for j in range(len(keys)):
-                    self.conf["service_logs"][keys[j]] = services_logs[i][okeys[j]]
-                cindex += 1
-                logger.warning(f"service_logs: {self.conf['service_logs']}")
-
-            self.conf["service_logs"]["length"] = str(cindex)
-            logger.info(f"service_logs: {self.conf['service_logs']}")
-
-        except Exception as e:
-            logger.error("ERROR in handle_outputs...")
-            logger.error(traceback.format_exc())
-            raise (e)
-
-    def get_secrets(self):
-        logger.info("get_secrets")
-        secrets={
-            "imagePullSecrets": self.local_get_file("/assets/pod_imagePullSecrets.yaml"),
-            "additionalImagePullSecrets": self.local_get_file("/assets/pod_additionalImagePullSecrets.yaml")
-        }
-        return secrets
 
 
 def {{cookiecutter.workflow_id |replace("-", "_")  }}(conf, inputs, outputs):  # noqa
